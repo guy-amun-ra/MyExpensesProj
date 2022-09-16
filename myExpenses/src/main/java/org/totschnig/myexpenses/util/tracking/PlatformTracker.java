43be59e49fc6d24273f1d113ef55645650a639e0
Michael Totschnig <michael@totschnig.org>
2022-09-16 10:27:12 +0200

    Without keep I'd expect R8 to remove this class?

--- myExpenses/src/main/java/org/totschnig/myexpenses/util/tracking/PlatformTracker.java
+++ myExpenses/src/main/java/org/totschnig/myexpenses/util/tracking/PlatformTracker.java
@@ -3,11 +3,14 @@ package org.totschnig.myexpenses.util.tracking;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.Keep;
+
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 import org.totschnig.myexpenses.util.distrib.DistributionHelper;
 import org.totschnig.myexpenses.util.Preconditions;
 
+@Keep
 public class PlatformTracker implements Tracker {
   private FirebaseAnalytics firebaseAnalytics;
   @Override
