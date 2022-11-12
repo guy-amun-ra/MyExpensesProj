d9091267822a37c1cc749f8f10e36d90f33c43e3
Michael Totschnig <michael@totschnig.org>
2022-11-12 22:09:20 +0100

    formatted

--- myExpenses/src/main/java/org/totschnig/myexpenses/util/licence/PlayStoreLicenceHandler.kt
+++ myExpenses/src/main/java/org/totschnig/myexpenses/util/licence/PlayStoreLicenceHandler.kt
@@ -67,7 +67,8 @@ open class PlayStoreLicenceHandler(
                     registerInventory(purchases, newPurchase)
 
                     if (newPurchase || oldStatus != licenceStatus || addOnFeatures.size > oldFeatures) {
-                        activity.onLicenceStatusSet(prettyPrintStatus(activity)
+                        activity.onLicenceStatusSet(
+                            prettyPrintStatus(activity)
                         )
                     }
                 }
@@ -126,7 +127,8 @@ open class PlayStoreLicenceHandler(
             }
         }
         handlePurchaseForAddOns(
-            inventory.filter { it.purchaseState == Purchase.PurchaseState.PURCHASED }.flatMap { it.products }.mapNotNull { Licence.parseFeature(it) },
+            inventory.filter { it.purchaseState == Purchase.PurchaseState.PURCHASED }
+                .flatMap { it.products }.mapNotNull { Licence.parseFeature(it) },
             newPurchase
         )
         licenseStatusPrefs.commit()
